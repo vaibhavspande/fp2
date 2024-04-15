e895f3cbc9e26d1a35fdcafc2ea9e912eafb5b5d
Vaibhav Pande <91242365+vaibhavspande@users.noreply.github.com>
2024-04-16 01:12:26 +0530

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -64,7 +64,7 @@ module.exports = {
     confirmations: 2,    // # of confs to wait between deployments. (default: 0)
     networkCheckTimeout: 10000,
     timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    skipDryRun: false     // Skip dry run before migrations? (default: false for public nets )
     },
     // Useful for private networks
     // private: {
